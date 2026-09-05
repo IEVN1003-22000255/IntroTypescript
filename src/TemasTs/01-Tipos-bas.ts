47c93e09d131876b80f32adcd91e261ca1cbbc7b
Andrestapia <82144@alumnos.utleon.edu.mx>
2026-09-04 23:18:41 -0600

    Tipos de datos 04/09/2026

---
+++ src/TemasTs/01-Tipos-bas.ts
@@ -0,0 +1,18 @@
+console.log("hola mundo");
+
+var num1=23;
+let num2=65;
+const num3=22;
+
+let nombre:string
+let num:number 
+let active:boolean
+let sum:number=num1+num2+num3
+console.log("la suma es: " +sum)
+
+nombre= ("Pedro");
+console.log(nombre);
+
+
+console.log(nombre);
+console.log("prueba");
